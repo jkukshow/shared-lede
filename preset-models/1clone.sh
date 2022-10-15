d021227c6c61aa5e861edf5ddd16e4e8c4d9d1d1
jkukshow <jkukshow@hotmail.com>
2022-10-15 10:34:37 +0800

    Update 1clone.sh

--- preset-models/1clone.sh
+++ preset-models/1clone.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # 下载源码
 git clone https://github.com/coolsnowwolf/lede.git
 mv ./lede/* ./
@@ -7,8 +8,13 @@ mv ./lede/* ./
 rm -rf package/lean/luci-theme-argon
 mkdir -p package/myapps
 cd package/myapps
+
+
+
+
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
+git clone https://github.com/kenzok8/small-package.git
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
